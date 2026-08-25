e75679fb7ea69f3cac4bc5f1a264a5135f0cd14c
Shivaditya Sarkar <sarkar.shivaditya@gmail.com>
2026-08-25 21:30:20 +0530

    Restore base styling and isolate emergency styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import { AuthProvider } from './auth'
 import './styles.css'
+import './emergency.css'
 import './cta.css'
 
 createRoot(document.getElementById('root')!).render(
